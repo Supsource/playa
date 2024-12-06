1da81ee6bbca2e138516b5c91ade09db4753be0d
Supriyo <supriyomalstartup@gmail.com>
2024-12-06 13:53:31 +0530

    added venues to book screen v1

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,9 +1,196 @@
-import { SafeAreaView, StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, Image, TextInput, Pressable, FlatList } from 'react-native'
 import React from 'react'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import VenueCard from '../components/VenueCard';
 
 const BookScreen = () => {
+  const venues =[
+    {
+      id: '0',
+      name: "DDSA - St.Joseph's Boys' High School (European)",
+      address: 'Ashok Nagar',
+      newImage:
+        'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800',
+      image:
+        'https://media.hudle.in/photos/47397',
+      location:
+        'No. 27, Museum Rd, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka',
+      rating: 3.6,
+      timings: '5.30 AM - 9:00 PM',
+      sportsAvailable: [
+        {
+          id: '10',
+          name: 'Badminton',
+          icon: 'badminton',
+          price: 500,
+          courts: [
+            {
+              id: '10',
+              name: 'Standard synthetic court 1',
+            },
+            {
+              id: '11',
+              name: 'Standard synthetic court 2',
+            },
+            {
+              id: '12',
+              name: 'Standard synthetic court 3',
+            },
+          ],
+        },
+        {
+          id: '11',
+          name: 'Cricket',
+          icon: 'cricket',
+          price: 1100,
+          courts: [
+            {
+              id: '10',
+              name: 'Full Pitch 1',
+            },
+            {
+              id: '11',
+              name: 'Full Pitch 2',
+            },
+          ],
+        },
+        {
+          id: '12',
+          name: 'Tennis',
+          icon: 'tennis',
+          price: 900,
+          courts: [
+            {
+              id: '10',
+              name: 'Court 1',
+            },
+            {
+              id: '11',
+              name: 'Court 2',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: '1',
+      image:
+        'https://playo.gumlet.io/PANCHAJANYABADMINTONFITNESSACADEMY/panchajanyabadmintonfitnessacademy1597334767773.jpeg?mode=crop&crop=smart&h=200&width=450&q=40&format=webp',
+      name: 'Panchajanya Badminton & Fitness Academy',
+      address: 'Kittur Rani Chennamma Stadium',
+      location:
+        'Gate 01, Kittur Rani Chennamma Stadium, Sports Complex, Jayanagar, Jayanagar East, Byrasandra, Jayanagar, Bengaluru, Karnataka - 560011',
+      rating: 4.0,
+      newImage:
+        'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800',
+      timings: '5 AM - 10 PM',
+      sportsAvailable: [
+        {
+          id: '10',
+          name: 'Badminton',
+          icon: 'badminton',
+          price: 500,
+          courts: [
+            {
+              id: '10',
+              name: 'Standard synthetic court 1',
+            },
+            {
+              id: '11',
+              name: 'Standard synthetic court 2',
+            },
+            {
+              id: '12',
+              name: 'Standard synthetic court 3',
+            },
+          ],
+        },
+        {
+          id: '11',
+          name: 'Cricket',
+          icon: 'cricket',
+          price: 1100,
+          courts: [
+            {
+              id: '10',
+              name: 'Full Pitch 1',
+            },
+            {
+              id: '11',
+              name: 'Full Pitch 2',
+            },
+          ],
+        },
+        {
+          id: '12',
+          name: 'Tennis',
+          icon: 'tennis',
+          price: 900,
+          courts: [
+            {
+              id: '10',
+              name: 'Court 1',
+            },
+            {
+              id: '11',
+              name: 'Court 2',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Sportexx',
+      image:
+        'https://playo.gumlet.io/SPORTEXX20220319100016960702/sportexx1647683524186.jpg?mode=crop&crop=smart&h=200&width=450&q=40&format=webp',
+      address: 'Hebbal Kempapura',
+      location: '#43/2, 3rd Cross, Sonnappa Layout, Bhuvaneshwari Nagar',
+      rating: 4.1,
+      newImage:
+        'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800',
+      timings: '5.30 AM - 9:00 PM',
+      sportsAvailable: [
+        {
+          id: '10',
+          name: 'Badminton',
+          icon: 'badminton',
+          price: 500,
+          courts: [
+            {
+              id: '10',
+              name: 'Standard synthetic court 1',
+            },
+            {
+              id: '11',
+              name: 'Standard synthetic court 2',
+            },
+            {
+              id: '12',
+              name: 'Standard synthetic court 3',
+            },
+          ],
+        },
+        {
+          id: '11',
+          name: 'Cricket',
+          icon: 'cricket',
+          price: 1100,
+          courts: [
+            {
+              id: '10',
+              name: 'Full Pitch 1',
+            },
+            {
+              id: '11',
+              name: 'Full Pitch 2',
+            },
+          ],
+        },
+      ],
+    },
+  ]
   return (
     <SafeAreaView>
      <View style={{flexDirection:"row", alignItems:"center", justifyContent:'space-between', padding: 12}}>
@@ -27,6 +214,24 @@ const BookScreen = () => {
       <TextInput placeholder='Search for Venues'/>
       <Ionicons name="search" size={24} color="gray" />
      </View>
+        <Pressable style={{flexDirection:'row', alignItems:"center", gap: 10,padding:13}}>
+          <View style={{padding: 10, borderRadius:10, borderColor: '#E0E0E0', borderWidth: 1}}>
+            <Text>Sports and Availability</Text>
+            </View>
+            <View style={{padding: 10, borderRadius:10, borderColor: '#E0E0E0', borderWidth: 1}}> 
+            <Text>Favorites</Text>
+            </View>
+            <View>
+            <Text style={{padding: 10, borderRadius:10, borderColor: '#E0E0E0', borderWidth: 1}}>Offers</Text>
+            </View>
+        </Pressable>
+        <FlatList 
+  data={venues} 
+  renderItem={({item}) => <VenueCard item={item} />}
+  contentContainerStyle={{paddingBottom: 20}}
+  showsHorizontalScrollIndicator={false}
+  keyExtractor={(item) => item.id}
+/>
     </SafeAreaView>
   )
 }

87d9b8d3a443fdf64b54d5d392c3e789877eb0d2
Supriyo <supriyomalstartup@gmail.com>
2024-12-22 17:03:37 +0530

    Create SelectImgScreen.js

---
+++ screens/SelectImgScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SelectImgScreen = () => {
+  return (
+    <View>
+      <Text>SelectImgScreen</Text>
+    </View>
+  )
+}
+
+export default SelectImgScreen
+
+const styles = StyleSheet.create({})

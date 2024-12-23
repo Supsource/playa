122d62b2c3325caea0377c472c28d6824bf79248
Supriyo <supriyomalstartup@gmail.com>
2024-12-23 19:19:00 +0530

    dev : developed first version of start screen, with maps

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,14 +1,124 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import MapView, {Callout, Marker} from 'react-native-maps';
 
 const StartScreen = () => {
+  const mapView = useRef(null);
+  const users = [
+    {
+      image:
+        'https://images.pexels.com/photos/7208625/pexels-photo-7208625.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '1',
+      latitude: '13.1295',
+      longitude: '79.5977',
+      name: 'sujan',
+      description: 'Hey!',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/2913125/pexels-photo-2913125.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '2',
+      latitude: '13.155',
+      longitude: '77.6070',
+      name: 'suhas',
+      description: "let's play",
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1042140/pexels-photo-1042140.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '3',
+      latitude: '13.0977',
+      longitude: '77.5839',
+      name: 'ashish',
+      description: "I'm always",
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/4307678/pexels-photo-4307678.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '4',
+      latitude: '13.0490',
+      longitude: '77.5936',
+      name: 'abhi',
+      description: 'At 8pm?',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1379031/pexels-photo-1379031.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '5',
+      latitude: '13.0623',
+      longitude: '77.5871',
+      name: 'akash',
+      description: 'Hey!',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/3264235/pexels-photo-3264235.jpeg?auto=compress&cs=tinysrgb&w=800',
+      id: '6',
+      latitude: '13.0354',
+      longitude: '77.5988',
+      name: 'Preetham',
+      description: 'What up?',
+    },
+  ];
+  useEffect(() => {
+    mapView.current.fitToCoordinates(circularPoints, {
+      edgePadding: {
+        top: 70,
+        bottom: 70,
+        left: 70,
+        right: 70,
+      },
+    });
+  }, []);
+
+  const BANGALORE_COORDS = {
+    latitude: 12.9916987,
+    longitude: 77.5945627,
+  };
+
+  const generateCircularPoints = (center, radius, numPoints) => {
+    const points = [];
+    const angleStep = (2 * Math.PI) / numPoints;
+
+    for (let i = 0; i < numPoints; i++) {
+      const angle = i * angleStep;
+      const latitude = center.latitude + (radius / 111) * Math.cos(angle);
+      const longitude =
+        center.longitude +
+        (radius / (111 * Math.cos(center.latitude))) * Math.sin(angle);
+      points.push({latitude, longitude});
+    }
+
+   return points;
+  };
+
+  const radius = 5;
+  const numPoints = 6;
+
+  const circularPoints = generateCircularPoints(
+    BANGALORE_COORDS,
+    radius,
+    numPoints,
+  );
+
+  console.log(circularPoints);
   return (
-    <View>
-      <Text>StartScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <MapView
+        ref={mapView}
+        style={{width: '100%', height: 400}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default StartScreen
+export default StartScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

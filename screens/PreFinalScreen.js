788ba495d2d488926c727465a1283290da3b9682
Supriyo <supriyomalstartup@gmail.com>
2024-12-28 20:56:12 +0530

    connect frontend with backend

--- screens/PreFinalScreen.js
+++ screens/PreFinalScreen.js
@@ -1,19 +1,109 @@
-import { Pressable, View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useContext, useState} from 'react';
+import axios from 'axios';
+import {AuthContext} from '../AuthContext';
+import { getRegistrationProgress } from '../registrationUtils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 const PreFinalScreen = () => {
+  const {token, setToken} = useContext(AuthContext);
+  const [userData, setUserData] = useState();
+  const navigation = useNavigation();
+  useEffect(() => {
+    if (token) {
+      navigation.replace('MainStack', {screen: 'Main'});
+    }
+  }, [token]);
+  useEffect(() => {
+    getAllUserData();
+  }, []);
+  const getAllUserData = async () => {
+    try {
+      const screens = [
+        'Register',
+        'Password',
+        'Name',
+        'Image',
+      ];
+
+      let userData = {};
+
+      for (const screenName of screens) {
+        const screenData = await getRegistrationProgress(screenName);
+        if (screenData) {
+          userData = {...userData, ...screenData};
+        }
+      }
+
+      setUserData(userData);
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+  const clearAllScreenData = async () => {
+    try {
+      const screens = [
+        'Register',
+        'Password',
+        'Name',
+        'Image',
+      ];
+
+      for (const screenName of screens) {
+        const key = `registration_progress_${screenName}`;
+        await AsyncStorage.removeItem(key);
+      }
+
+      console.log('All screen data cleared!');
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+
+  console.log(userData)
+  const registerUser = async () => {
+    try {
+      const response = await axios
+        .post('http://localhost:3000/register', userData)
+        .then(response => {
+          console.log(response);
+          const token = response.data.token;
+          AsyncStorage.setItem('token', token);
+          setToken(token);
+        });
+
+      clearAllScreenData();
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <View style={{marginTop: 80}}>
-        <Text style={{fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}>
-          Congratulations! 🎉
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+          }}>
+          All set to register
         </Text>
-        <Text style={{marginTop: 10, textAlign: 'center'}}>
-          All set to register, Setting up your profile.
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+            marginTop: 10,
+          }}>
+          Setting up your profile for you
         </Text>
       </View>
+
       <Pressable
-        // onPress={registerUser}
+        onPress={registerUser}
         style={{backgroundColor: '#03C03C', padding: 15, marginTop: 'auto'}}>
         <Text
           style={{
@@ -26,7 +116,9 @@ const PreFinalScreen = () => {
         </Text>
       </Pressable>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default PreFinalScreen
+export default PreFinalScreen;
+
+const styles = StyleSheet.create({});

5b59083a4f50690004bf55123a17afa12b80e7ff
Supriyo <supriyomalstartup@gmail.com>
2024-12-28 20:37:49 +0530

    developed post methods

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const moment = require('moment');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +13,7 @@ const dbUri = process.env.DB_URI;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const jwt = require('jsonwebtoken');
 
 mongoose.connect(dbUri)
   .then(() => {
@@ -22,4 +25,43 @@ mongoose.connect(dbUri)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+const User = require('./models/user');
+const Game = require('./models/game');
+const Venue = require('./models/venue');
+
+app.post('/register', async (req, res) => {
+  try {
+    const userData = req.body;
+    const newUser = new User(userData);
+    await newUser.save();
+    const secretKey = crypto.randomBytes(32).toString('hex');
+    const token = jwt.sign({userId: newUser._id}, secretKey);
+    res.status(200).json({token});
+  } catch (error) {
+    console.log('Error creating user', error);
+    res.status(500).json({error: 'Internal server error'});
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+    if (!user) {
+      return res.status(401).json({message: 'Invalid email'});
+    }
+    if (user.password !== password) {
+      return res.status(401).json({message: 'Invalid password'});
+    }
+
+    const secretKey = crypto.randomBytes(32).toString('hex');
+    const token = jwt.sign({userId: user._id}, secretKey);
+    res.status(200).json({token});
+  } catch (error) {
+    console.log('error loggin in', error);
+    res.status(500).json({message: 'Error loggin In'});
+  }
 });

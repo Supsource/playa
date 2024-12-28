2a71b7a397b4864ee81eb070e62979e266b46b00
Supriyo <supriyomalstartup@gmail.com>
2024-12-28 18:52:16 +0530

    added token

--- AuthContext.js
+++ AuthContext.js
@@ -1,48 +1,40 @@
-import {createContext, useEffect, useState} from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {jwtDecode} from 'jwt-decode';
-import 'core-js/stable/atob';
+import jwtDecode from 'jwt-decode';
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
   const [token, setToken] = useState('');
   const [userId, setUserId] = useState('');
-  const [isLoading, setIsLoading] = useState('');
-
+  const [isLoading, setIsLoading] = useState(false);
   const [upcomingGames, setUpcomingGames] = useState([]);
 
   const isLoggedIn = async () => {
     try {
       setIsLoading(true);
       const userToken = await AsyncStorage.getItem('token');
-      setToken(userToken);
-      setIsLoading(false);
+      if (userToken) {
+        setToken(userToken);
+        const decodedToken = jwtDecode(userToken);
+        setUserId(decodedToken.userId);
+      }
     } catch (error) {
-      console.log('error', error);
+      console.error('Error checking login status', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      const token = await AsyncStorage.getItem('token');
-      const decodedToken = jwtDecode(token);
-      const userId = decodedToken.userId;
-      setUserId(userId);
-    };
-
-    fetchUser();
-  }, []);
-  useEffect(() => {
     isLoggedIn();
-  }, [token]);
+  }, []);
 
   return (
-    <AuthContext.Provider
-      value={{token, isLoading, setToken, userId, setUserId,upcomingGames,setUpcomingGames}}>
+    <AuthContext.Provider value={{ token, userId, isLoading, upcomingGames, setUpcomingGames }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export {AuthContext, AuthProvider};
+export { AuthContext, AuthProvider };

b1c8ed0a793753840e2b39b65c25c7f5bdda9d32
Supriyo <supriyomalstartup@gmail.com>
2024-12-29 22:08:51 +0530

    dev: initial GameSetUp Screen

---
+++ screens/GameSetUpScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GameSetUpScreen = () => {
+  return (
+    <View>
+      <Text>GameSetUpScreen</Text>
+    </View>
+  )
+}
+
+export default GameSetUpScreen
